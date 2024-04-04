fbde5f362919203488a80b0249ebd14e16061444
Zyie <24736175+Zyie@users.noreply.github.com>
2024-04-04 20:21:15 +0100

    Fix: devtool types (#10)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,27 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../global.d.ts" />
-import { Devtools } from './types';
+import { Devtools, DevtoolsAPI } from './types';
 
 export * from './types';
 
-export async function initDevtools(opts: Devtools) {
-  if (!opts.app && !opts.renderer && !opts.stage) {
+export async function initDevtools(opts: DevtoolsAPI) {
+  const castOpts = opts as Devtools;
+
+  if (!castOpts.app && !castOpts.renderer && !castOpts.stage) {
     throw new Error('You must provide either an app or a renderer and stage');
   }
-  if (opts.app) {
-    opts.renderer = opts.app.renderer as import('pixi.js').Renderer;
-    opts.stage = opts.app.stage;
+  if (castOpts.app) {
+    castOpts.renderer = castOpts.app.renderer as import('pixi.js').Renderer;
+    castOpts.stage = castOpts.app.stage;
   }
 
-  if (!opts.pixi) {
-    opts.pixi = await import('pixi.js');
+  if (!castOpts.pixi) {
+    castOpts.pixi = await import('pixi.js');
   }
 
   window.__PIXI_DEVTOOLS__ = {
-    pixi: opts.pixi,
-    app: opts.app,
-    stage: opts.stage,
-    renderer: opts.renderer,
-    plugins: opts.plugins,
+    pixi: castOpts.pixi,
+    app: castOpts.app,
+    stage: castOpts.stage,
+    renderer: castOpts.renderer,
+    plugins: castOpts.plugins,
   };
 }
