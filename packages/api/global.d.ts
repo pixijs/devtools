5c727a01046605f316f8345cebbf96c0a0cf3009
Zyie <24736175+Zyie@users.noreply.github.com>
2024-04-08 15:09:16 +0100

    Fix: window types (#19)
    * Fix: contains property check
    
    * Fix: window types

--- packages/api/global.d.ts
+++ packages/api/global.d.ts
@@ -3,8 +3,8 @@ declare global {
     __PIXI_APP__: import('pixi.js').Application | undefined;
     __PIXI_STAGE__: import('pixi.js').Container | undefined;
     __PIXI_RENDERER__: import('pixi.js').Renderer | undefined;
-    __PIXI__: import('pixi.js');
-    PIXI: import('pixi.js');
+    __PIXI__: typeof import('pixi.js');
+    PIXI: typeof import('pixi.js');
     __PIXI_DEVTOOLS_WRAPPER__: any;
     __PIXI_DEVTOOLS__: {
       pixi: typeof import('pixi.js');
@@ -12,8 +12,8 @@ declare global {
       stage?: import('pixi.js').Container | undefined;
       renderer?: import('pixi.js').Renderer | undefined;
       plugins?: {
-        stats?: import('./types').NodeTrackerPlugin[];
-        properties?: import('./types').PropertyPlugin[];
+        stats?: import('./dist/types').NodeTrackerPlugin[];
+        properties?: import('./dist/types').PropertyPlugin[];
       };
     };
     $pixi: import('pixi.js').Container | null;
