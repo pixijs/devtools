e7e2ca9cb8e5d21dd82f4555e20f09fde83e4035
Zyie <24736175+Zyie@users.noreply.github.com>
2024-05-29 14:19:19 +0100

    chore: update locale example

--- packages/devtool-local/src/main.tsx
+++ packages/devtool-local/src/main.tsx
@@ -1,56 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from '@devtool/frontend/App';
 import type { BridgeFn } from '@devtool/frontend/lib/utils';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PixiDevtools } from '../../devtool-chrome/src/inject/pixi';
+import scene from './scene.ts';
+import * as PIXI from 'pixi.js';
 
-// {
-//   id: 'root',
-//   name: 'root',
-//   children: [
-//     {
-//       id: 'child1',
-//       name: 'child1',
-//       children: [
-//         {
-//           id: 'child1-1',
-//           name: 'child1-1',
-//           children: [],
-//           metadata: {
-//             type: 'Sprite',
-//             uid: 'child1-1',
-//           },
-//         },
-//       ],
-//       metadata: {
-//         type: 'Container',
-//         uid: 'child1',
-//       },
-//     },
-//     {
-//       id: 'child2',
-//       name: 'child2',
-//       children: [
-//         {
-//           id: 'child2-1',
-//           name: 'child2-1',
-//           children: [],
-//           metadata: {
-//             type: 'Sprite',
-//             uid: 'child2-1',
-//           },
-//         },
-//       ],
-//       metadata: {
-//         type: 'Container',
-//         uid: 'child2',
-//       },
-//     },
-//   ],
-//   metadata: {
-//     type: 'Container',
-//     uid: 'root',
-//   },
-// },
+(async () => {
+  // wait for the app to be ready
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+  const app = await scene(canvas);
+
+  window.__PIXI_DEVTOOLS__ = {
+    pixi: PIXI,
+    app,
+  };
+
+  const renderer = PixiDevtools.renderer;
+
+  if (renderer) {
+    renderer.render = new Proxy(renderer.render, {
+      apply(target, thisArg, ...args) {
+        PixiDevtools.update();
+        messageListeners.forEach((listener) =>
+          listener({
+            method: 'pixi-state-update',
+            data: JSON.stringify(PixiDevtools.state),
+          }),
+        );
+        // @ts-expect-error - TODO: fix this type
+        return target.apply(thisArg, ...args);
+      },
+    });
+  }
+})();
 
 const messageListeners: ((message: unknown) => void)[] = [];
 const mockChrome = {
@@ -75,152 +61,21 @@ const mockChrome = {
   },
 } as unknown as typeof chrome;
 
-const mockBridge: BridgeFn = (): Promise<any> => {
-  // eval(code);
+const mockBridge: BridgeFn = (code): Promise<any> => {
+  eval(code);
   return Promise.resolve();
 };
 
-let dt = 0;
-const dataA = {
-  fps: 60,
-};
-const dataB = {
-  fps: 30,
-  other: 30,
-};
-let currentData = dataA;
-const scene = {
-  id: 'root',
-  name: 'root',
-  children: [] as any[],
-  metadata: {
-    type: 'Container',
-    uid: 'root',
-  },
-};
-// create 100 nodes
-let prevNode = scene;
-for (let i = 0; i < 10; i++) {
-  const node = {
-    id: `node-${i}`,
-    name: `node-${i}`,
-    metadata: {
-      type: 'Container',
-      uid: 'root',
-    },
-    readOnly: false,
-    children: [],
-  };
-  prevNode.children.push(node);
-  prevNode = node;
-}
-// every 1 second, send a message to the devtools
-setInterval(() => {
-  // every 3 seconds change the stats
-  dt += 1;
-
-  let stats = currentData;
-  if (dt % 6 === 0) {
-    stats = currentData = currentData === dataA ? dataB : dataA;
-  }
-
-  messageListeners.forEach((listener) => {
-    listener({
-      method: 'pixi-state-update',
-      data: JSON.stringify({
-        version: '8.0.0',
-        stats,
-        selectedNode: 'node-1',
-        activeProps: [
-          {
-            value: [0, 100],
-            prop: 'position',
-            entry: {
-              section: 'Transform',
-              type: 'vector2',
-              options: {
-                label: 'Position',
-                x: {
-                  label: 'x',
-                },
-                y: {
-                  label: 'y',
-                },
-              },
-            },
-          },
-          {
-            value: 0,
-            prop: 'width',
-            entry: {
-              section: 'Section 2',
-              label: 'Width',
-              type: 'number',
-              options: {},
-            },
-          },
-          {
-            value: true,
-            prop: 'width',
-            entry: {
-              section: 'Section 2',
-              label: 'Width',
-              type: 'boolean',
-              options: {},
-            },
-          },
-          {
-            value: 0.5,
-            prop: 'height and things',
-            entry: {
-              section: 'Section 2',
-              type: 'range',
-              options: {
-                min: 0,
-                max: 1,
-                step: 0.05,
-              },
-            },
-          },
-          {
-            value: 'a',
-            prop: 'helloWorld',
-            entry: {
-              section: 'Section 2',
-              type: 'select',
-              options: {
-                options: ['a', 'b', 'c'],
-              },
-            },
-          },
-          {
-            value: 0xff0000,
-            prop: 'tint',
-            entry: {
-              section: 'Section 2',
-              type: 'color',
-            },
-          },
-          {
-            value: [1, 2, 3, 4],
-            prop: 'bounds',
-            entry: {
-              section: 'Section 2',
-              type: 'vectorX',
-              options: {
-                inputs: [{ label: 'x' }, { label: 'y' }, { label: 'width' }, { label: 'height' }],
-              },
-            },
-          },
-        ],
-        sceneGraph: scene,
-      }),
-    });
-  });
-}, 1000);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App bridge={mockBridge} chromeProxy={mockChrome} />
+    <div
+      id="root"
+      style={{ display: 'flex', flexDirection: 'column', maxHeight: '100vh', alignItems: 'center' }}
+    >
+      <canvas id="canvas" style={{ width: '600px', height: '450px' }} />
+      <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+        <App bridge={mockBridge} chromeProxy={mockChrome} />
+      </div>
+    </div>
   </React.StrictMode>,
 );
