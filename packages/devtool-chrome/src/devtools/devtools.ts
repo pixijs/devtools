a19e6e15599f1eb8835dceb7d6415d0c4e1577ed
Jake Yallop <30874283+JakeYallop@users.noreply.github.com>
2026-03-06 20:16:43 +0000

    Revert unnecessary changes

--- packages/devtool-chrome/src/devtools/devtools.ts
+++ packages/devtool-chrome/src/devtools/devtools.ts
@@ -1,18 +1,21 @@
 import { DevtoolMessage } from '@devtool/frontend/types';
 import { convertPostMessage } from '../messageUtils';
 
-const title = import.meta.env.DEV ? 'Dev: PixiJS DevTools' : 'PixiJS DevTools';
+chrome.devtools.panels.create(
+  import.meta.env.DEV ? 'Dev: PixiJS DevTools' : 'PixiJS DevTools',
+  'pixi-icon-active-128.png',
+  'devtools/panel/panel.html',
+  (panel) => {
+    const tabId = chrome.devtools.inspectedWindow.tabId;
 
-chrome.devtools.panels.create(title, 'pixi-icon-active-128.png', 'devtools/panel/panel.html', (panel) => {
-  const tabId = chrome.devtools.inspectedWindow.tabId;
+    panel.onShown.addListener(() => {
+      const message = convertPostMessage(DevtoolMessage.panelShown, {});
+      chrome.runtime.sendMessage({ ...message, tabId });
+    });
 
-  panel.onShown.addListener(() => {
-    const message = convertPostMessage(DevtoolMessage.panelShown, {});
-    chrome.runtime.sendMessage({ ...message, tabId });
-  });
-
-  panel.onHidden.addListener(() => {
-    const message = convertPostMessage(DevtoolMessage.panelHidden, {});
-    chrome.runtime.sendMessage({ ...message, tabId });
-  });
-});
+    panel.onHidden.addListener(() => {
+      const message = convertPostMessage(DevtoolMessage.panelHidden, {});
+      chrome.runtime.sendMessage({ ...message, tabId });
+    });
+  },
+);
